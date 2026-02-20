4ce8899f76a24742495b190528ed3f061f7aa0f3
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-02-20 20:13:39 +0800

    updating the dashboard

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="../global.css">
 </head>
-<body>
-    <?php include (__DIR__ . '/../components/navigation.php'); // __DIR__ provides the absolute filesystem path of the directory containing the current file.
-    include (__DIR__ . '/../components/searchbar.php');
+
+<body class="min-h-screen flex flex-col">
+    <?php include(__DIR__ . '/../components/navigation.php'); // __DIR__ provides the absolute filesystem path of the directory containing the current file.
+    include(__DIR__ . '/../components/searchbar.php');
     ?>
+    <div class="flex-1 mx-[5px] mb-[5px] px-4 py-24 bg-white rounded-lg shadow-md flex flex-col items-center justify-center">
+        <h1 class="text-3xl font-bold text-center">Welcome to your Dashboard</h1>
+        <p class="text-center mt-4 text-gray-600">Here you can manage your bookings, reservations, and more.</p>
+    </div>
 </body>
+
 </html>
