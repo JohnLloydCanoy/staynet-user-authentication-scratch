e30f5228e7a27d39caa242223a7f50f8ffcaab1b
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-02-20 18:16:45 +0800

    adding the tailwind for easy styling

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ echo "Welcome to StayNet!";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="global.css?v=<?php echo time(); ?>">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     <h1>Hello World!</h1>
