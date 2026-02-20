fa304b004f60a8c1a7fdd2c98ef66ddc645f1214
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-02-20 20:25:41 +0800

    update

---
+++ components/buttons/primary_button.php
@@ -0,0 +1,3 @@
+<a href="<?php echo $btn_link ?? '#'; ?>" class="inline-flex justify-center items-center px-6 py-2.5 bg-[#4f46e5] hover:bg-[#4338ca] text-white text-sm font-bold tracking-wide rounded-md transition-colors duration-200 shadow-sm cursor-pointer w-full md:w-auto">
+    <?php echo $btn_text ?? 'Submit'; ?>
+</a>
