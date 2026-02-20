13b039289dc693f8756f94242d1f4e2dc96d4124
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-02-20 20:27:27 +0800

    creating secondary reuasble button

---
+++ components/buttons/secondary_button.php
@@ -0,0 +1,3 @@
+<a href="<?php echo $btn_link ?? '#'; ?>" class="inline-flex justify-center items-center px-6 py-2.5 bg-white border-2 border-[#4f46e5] text-[#4f46e5] hover:bg-gray-50 text-sm font-bold tracking-wide rounded-md transition-colors duration-200 shadow-sm cursor-pointer w-full md:w-auto">
+    <?php echo $btn_text ?? 'Cancel'; ?>
+</a>
