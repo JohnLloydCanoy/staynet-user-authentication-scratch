1f40f6b17336df7c6ed9f5f0aa0016646cd0e3f4
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-02-20 18:22:23 +0800

    update

--- index.php
+++ index.php
@@ -8,9 +8,7 @@ echo "Welcome to StayNet!";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="global.css?v=<?php echo time(); ?>">
-    <script src="https://cdn.tailwindcss.com"></script>
+    <title>StayNet</title>
 </head>
 <body>
     <h1>Hello World!</h1>
