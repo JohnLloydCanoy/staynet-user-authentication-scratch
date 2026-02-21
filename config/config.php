e90991315399011421692040093eaa8aa419566d
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-02-21 17:06:56 +0800

    Connection options for better security and error handling

--- config/config.php
+++ config/config.php
@@ -11,4 +11,10 @@
     $charset = 'utf8mb4';
     // Data Source Name (DSN) for PDO
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    
+    $options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
 ?>
