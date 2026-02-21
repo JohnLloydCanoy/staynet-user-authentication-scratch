d94360c8a06d756d64dba47f4ead4293473cccf5
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-02-21 16:08:05 +0800

    updating

---
+++ components/buttons/submit_button.php
@@ -0,0 +1,3 @@
+<button type="submit" class="w-full flex justify-center items-center px-6 py-3 mt-6 bg-[#4f46e5] hover:bg-[#4338ca] text-white text-sm font-bold tracking-wide rounded-md transition-colors duration-200 shadow-sm cursor-pointer">
+    <?php echo $btn_text ?? 'Submit'; ?>
+</button>
