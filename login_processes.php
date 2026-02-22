cded0a5b2c978a17dea469766282242c3f6b4f3d
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-02-22 18:56:41 +0800

    Find the user in the database by their email

--- login_processes.php
+++ login_processes.php
@@ -6,4 +6,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $email = $_POST['email'];
     $password = $_POST['password'];
+    try {
+        
+        $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+        $stmt->execute([':email' => $email]);
 ?>
