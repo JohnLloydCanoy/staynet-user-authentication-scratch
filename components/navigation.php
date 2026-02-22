15e87ae99846d65399b50df2ada9ef0e6d16ecfc
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-02-22 17:38:13 +0800

    creating the host dashboard

--- components/navigation.php
+++ components/navigation.php
@@ -1,14 +1,14 @@
 <nav class="w-full  px-8 py-5 flex justify-between items-center shadow-sm">
 
-    <a href="index.php" class="text-2xl font-black tracking-wide">
+    <a href="/staynet-user-authentication-scratch/index.php" class="text-2xl font-black tracking-wide">
         <span class="text-black">STAY</span><span class="text-[#4f46e5]">NET</span>
     </a>
 
     <div class="flex items-center space-x-10 text-black font-bold text-sm tracking-wider">
         <a href="#" class="uppercase hover:text-[#4f46e5] transition-colors duration-200">Book</a>
         <a href="#" class="uppercase hover:text-[#4f46e5] transition-colors duration-200">Reservation</a>
-        <a href="#" class="uppercase hover:text-[#4f46e5] transition-colors duration-200">Log In</a>
-        <a href="sign_in.php" class="ml-4 hover:text-[#4f46e5] transition-colors duration-200">
+        <a href="/staynet-user-authentication-scratch/login.php" class="uppercase hover:text-[#4f46e5] transition-colors duration-200">Log In</a>
+        <a href="/staynet-user-authentication-scratch/sign_up.php" class="ml-4 hover:text-[#4f46e5] transition-colors duration-200">
             Become a Host
         </a>
     </div>
