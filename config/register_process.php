0735e08cabdf7c7b709ffea3174db529c17212fa
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-02-22 17:54:13 +0800

    capturing the form data

--- config/register_process.php
+++ config/register_process.php
@@ -1,3 +1,10 @@
 <?php
 require_once 'config/config.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $full_name = $_POST['full_name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $role = $_POST['role']; // 'guest' or 'host'
+}
 ?>
